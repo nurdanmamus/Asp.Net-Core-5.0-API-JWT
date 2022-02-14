c6e3ed94a726bece9a402d87e38483c1fb4e1c29
Nurdan Mamuş <nurdanmamus@gmail.com>
2022-02-14 12:49:15 +0300

    setting ConfigureServices(Startup.cs)

--- AuthServer.API/Startup.cs
+++ AuthServer.API/Startup.cs
@@ -1,8 +1,18 @@
 ﻿using AuthServer.Core.Configurations;
+using AuthServer.Core.Models;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using AuthServer.Data;
+using AuthServer.Data.Repositories;
+using AuthServer.Service.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,10 +38,62 @@ namespace AuthServer.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //DI container'a bir nesne geçtik 
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ITokenService, TokenService>(); 
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped(typeof(IServiceGeneric<,>),typeof(ServiceGeneric<,>)); 
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(options => 
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"),
+                    sqlOptions=> {
+                        sqlOptions.MigrationsAssembly("AuthServer.Data"); 
+                    });   
+            });
+
+            services.AddIdentity<UserApp, IdentityRole>(option => 
+             {
+                 option.User.RequireUniqueEmail = true;
+                 option.Password.RequireNonAlphanumeric = false; 
+             }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            //DI container'a bir nesne geçtik  
             //Options pattern -> DI üzerinden appsetting'deki datalara ulaşmaya denir.
-            services.Configure<CustomTokenOption>(Configuration.GetSection("TokenOption"));
-            services.Configure<Client>(Configuration.GetSection("Client"));
+            services.Configure<CustomTokenOption>(Configuration.GetSection("TokenOption")); 
+           
+            services.Configure<List<Client>>(Configuration.GetSection("Client"));
+
+            //productcontrollera istek yapıldığında token doğrulama da yapıyor
+            //hem token üreten hem de doğrulama yapaım bir api
+            services.AddAuthentication(option =>
+            {
+                //birden fazla üyelik sistemi olan api'lerde
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;            
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,opts=> {
+                //cookie bazlı mı yoksa jwt bazlı mı yapılacak? jwt olduğunu burada yazıyoruz
+                //TokenValidationParameters ile doğrulamada geçerli olacak parametreleri belirtiyoruz.
+                //customtokenoption'a mapleme
+                var tokenOptions = Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+                opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidIssuer = tokenOptions.Issuer, 
+                    ValidAudience = tokenOptions.Audience[0], 
+                    IssuerSigningKey = SignService.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer=true, 
+                    ValidateLifetime = true,
+                    ClockSkew=TimeSpan.Zero 
+                    //farklı serverlara kurduğumuz api'nin kurulan serverlar ile
+                    //saat farkını tölere etmek için 5 dk ekler. biz onu sıfıra 
+                };
+            });
+
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
